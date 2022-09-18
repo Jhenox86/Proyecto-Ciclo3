6024176bfaff60a5941b21a99a2f0f18bd4d0515
LianJules <huertasjulian64@gmail.com>
2022-09-18 13:43:34 -0500

    Implementacion plantilla SPRINGBOOT al proyecto

---
+++ src/test/java/com/utp/huertasjulian/main/JTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.huertasjulian.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
