bd35d16e3b8c6746ab22d6ba9f0702f0787e4839
Jhenox86 <jhenox@gmail.com>
2022-09-24 13:14:31 -0500

    Organizacion de estructura del proyecto 24-09-2022

--- src/main/java/com/utp/huertasjulian/main/controller/JTravelAplicationController.java
+++ src/main/java/com/utp/huertasjulian/main/controller/JTravelAplicationController.java
@@ -16,6 +16,31 @@ public class JTravelAplicationController {
         return goToIndex();
     }
 
+    @GetMapping("/browse.html")
+    public String goToBrowse(){
+        return "browse";
+    }
+
+    @GetMapping("/details.html")
+    public String goToDetails(){
+        return "details";
+    }
+
+    @GetMapping("/profile.html")
+    public String goToProfile(){
+        return "profile";
+    }
+
+    @GetMapping("/streams.html")
+    public String goToStreams(){
+        return "streams";
+    }
+
+    @GetMapping("/login.html")
+    public String goToLogin(){
+        return "login";
+    }
+
    
     
 
