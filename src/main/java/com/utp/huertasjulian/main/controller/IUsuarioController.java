43cbb00bc2af73e5156477d217c9116e853bd208
jsofia24 <jsofia24@hotmail.com>
2022-09-29 21:46:07 -0500

    Generación dependencias, creacion controller login

---
+++ src/main/java/com/utp/huertasjulian/main/controller/IUsuarioController.java
@@ -0,0 +1,5 @@
+package com.utp.huertasjulian.main.controller;
+
+public class IUsuarioController {
+    
+}
---
+++ src/main/java/com/utp/huertasjulian/main/controller/UsuarioController.java
@@ -0,0 +1,17 @@
+package com.utp.huertasjulian.main.controller;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.google.gson.Gson;
+import beans.Usuario;
+import connection.DBConection;
+
+public class UsuarioController {
+    // hora 2:22 clase grupo 44 120922
+    @Override
+    public String login (String username, String contrasena){
+        
+        Gson gson = new Gson();
+        DBConnection con = new DBConnection (); 
+    }
+}
