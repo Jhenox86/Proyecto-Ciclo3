bdc4a7e3999c77ae03ac96e2854e183a40b757a1
Jhenox86 <jhenox@gmail.com>
2022-10-01 16:02:42 -0500

    Se agrega conectivida BD y crud de reseña

--- prueba_py.py
+++ prueba_py.py
@@ -13,7 +13,7 @@ app= Flask (__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'sistema'
+app.config['MYSQL_DB'] = 'testdb'
 
 mysql = MySQL(app)
 
